4116208de592719de3eaee8c526892530d345229
sbsteacher <id@gmail.com>
2026-02-19 15:09:48 +0900

    main.js 작업

--- src/main.js
+++ src/main.js
@@ -2,13 +2,29 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+//폰트어썸
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(fas)
 
-app.use(createPinia())
+//부트스트랩
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia)
 app.use(router)
+app.use(BootstrapVue)
+app.use(IconsPlugin)
 
 app.mount('#app')
